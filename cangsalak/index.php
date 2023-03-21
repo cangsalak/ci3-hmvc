b5ab55e7cb699720294e96063b595669fb3c0418
cangsalak <max_kai@hotmail.co.th>
2023-03-21 14:02:27 +0700

    edit

--- cangsalak/index.php
+++ cangsalak/index.php
@@ -22,7 +22,7 @@ foreach (glob($_SESSION['path']) as $filename) {
 <!doctype html>
 <html>
     <head>
-        <title>Cangsalak Codeigniter CRUD Generator</title>
+        <title>Codeigniter CRUD Generator</title>
         
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -153,8 +153,7 @@ foreach (glob($_SESSION['path']) as $filename) {
                                     $config['base_url'] = $base_url;
                                 '</li>
                                 <li>$config['index_page'] = ''</li>
-                                <li>$config['url_suffix'] = '.html'</li>
-                                <li>$config['encryption_key'] = 'randomstring'</li>
+                                <li>$config['encryption_key'] = 'กำหนดตามใจชอบ'</li>
 
                             </ul>
 
